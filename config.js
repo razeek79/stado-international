e9b61a1be5c4719565cf6d721d61084350cbc528
razeek79 <razeekfariz6@gmail.com>
2026-04-11 16:23:43 +0530

    favicon added

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 
-const supabaseUrl = 'https://xdgcebrfaofawkdnqbuv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZ2NlYnJmYW9mYXdrZG5xYnV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMzA0NzYsImV4cCI6MjA4ODcwNjQ3Nn0.h-m5jpaTY-a6DRPCnJ73l3tYyPaTjwJShAQO2TIhUPU';
+const supabaseUrl = 'https://xdgcebrfaofawkd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI69sZSI6ImFub24iLCJpYXQiOjE3NzMxMzA0NzYsImV4cCI6MjA4ODcwNjQ3Nn0.h-m5jpaTY-a6DRPCnJ73l3tYyPaTjwJShAQO2TIhUPU';
 
 // ✅ Create ONLY ONE global instance
 if (!window.supabaseClient) {
