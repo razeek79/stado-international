6770b9654fdbf9d5482020dca68af5b2f5b38ade
razeek79 <razeekfariz6@gmail.com>
2026-06-05 14:43:49 +0530

    admission screenshot upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 
 // config.js
-const supabaseUrl = 'https://xdgceb***dnqbuv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOi***TjwJShAQO2TIhUPU';
+const supabaseUrl = 'https://xdgcebrfaofawkdnqbuv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZ2NlYnJmYW9mYXdrZG5xYnV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMzA0NzYsImV4cCI6MjA4ODcwNjQ3Nn0.h-m5jpaTY-a6DRPCnJ73l3tYyPaTjwJShAQO2TIhUPU';
 
 
 
